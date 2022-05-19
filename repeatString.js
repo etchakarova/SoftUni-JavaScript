99c62ee7fc0cd54efea9f9df0e4fae2e281b4da6
Elena Tchakarova <105879345+etchakarova@users.noreply.github.com>
2022-05-19 21:05:27 +0300

    Delete repeatString.js

--- repeatString.js
+++
@@ -1,9 +0,0 @@
-function repeatString(word, num) {
-    let newWord = ''
-    for (let i = 0; i < num; i++) {
-        newWord += word
-    }
-    return newWord
-}
-console.log(repeatString("abc", 3));
-console.log(repeatString("String", 2));
