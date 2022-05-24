a6b9b5e43eb1bf1d8fc5ccf2c300fc98a387e602
Elena Tchakarova <105879345+etchakarova@users.noreply.github.com>
2022-05-24 17:38:33 +0300

    Delete Lecture - Syntax, Functions and Statements directory

--- Lecture - Syntax, Functions and Statements/daysInAMonth.js
+++
@@ -1,6 +0,0 @@
-function daysInAMonth(month, year) {
-    return new Date(year, month, 0).getDate();
-}
-
-console.log(daysInAMonth(1, 2012));
-console.log(daysInAMonth(2, 2021));
--- Lecture - Syntax, Functions and Statements/stringLength.js
+++
@@ -1,7 +0,0 @@
-function stringLength(a, b, c) {
-  let sum = a.length + b.length + c.length;
-  let average = sum / 3;
-  console.log(sum);
-  console.log(Math.floor(average));
-}
-stringLength("chocolate", "ice cream", "cake");
