5f235a688e5178d6436429fe78aca40fc60c934b
Elena Tchakarova <105879345+etchakarova@users.noreply.github.com>
2022-05-24 17:38:43 +0300

    Delete Exercise - Syntax, Functions and Statements directory

--- Exercise - Syntax, Functions and Statements/fruit.js
+++
@@ -1,5 +0,0 @@
-function fruit(fruit, weight, money) {
-    console.log(`I need $${(money*(weight / 1000)).toFixed(2)} to buy ${(weight/1000).toFixed(2)} kilograms ${fruit}.`);
-}
-fruit('orange', 2500, 1.80);
-fruit('apple', 1563, 2.35);
